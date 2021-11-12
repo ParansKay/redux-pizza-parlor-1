1fb3b90fc51c87f5cb0524ec06e2430725fe9c6e
Hannah Fligel <fligelhannah@gmail.com>
2021-11-11 20:10:11 -0600

    comments added to index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+//import redux
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
-import logger from 'redux-logger';
+import logger from 'redux-logger'; //this essentially acts as a console log 
 import axios from 'axios';
 
 // reducer
+// creating a global variable "pizza", that is given a state and action 
+// every time an action takes place, it will run the reducer
 const pizza = (state = "I love pizza", action) => {
     console.log("I'm in pizza!", action);
     return state;

ebec86c1c7dadfbefd92f0fc808c42c4c13c241c
Hannah Fligel <fligelhannah@gmail.com>
2021-11-13 15:11:33 -0600

    mock post working from checkout

--- src/components/CheckOut/CheckOut.js
+++ src/components/CheckOut/CheckOut.js
@@ -3,12 +3,40 @@ import {useState, useEffect } from 'react';
 import "../Checkout/Checkout.css"
 import {Table}from 'react-bootstrap';
 import {Button} from '@material-ui/core'
+import axios from 'axios';
+
 
 
 function Checkout() {
   
   // const reducerName = useSelector(store => store.reducerName);
   const dispatch = useDispatch();
+
+  const[ newOrder, setNewOrder ]=useState({
+    customer_name: "Hannah", //waiting for store data
+    street_address: "hiawatha ave",//waiting for store data
+    city: "minneapolis",//waiting for store data
+    zip: "55406",//waiting for store data
+    type: "takeout",//waiting for store data
+    total: 20,//waiting for store data
+    pizzas: [{
+      id: 2, //waiting for store data
+      quantity: 3 //waiting for store data
+    }] 
+})
+
+  const addOrder = ()=>{
+    console.log( 'in addItem' );
+    axios.post( '/api/order', newOrder ).then ( (response)=>{
+        console.log('back from POST:', response.data);
+        alert('order added');
+    }).catch( (err)=>{
+        console.log(err);
+        alert('nope');
+    })
+}
+
+
 
   return (
     <div className="checkout-page">
@@ -33,18 +61,22 @@ function Checkout() {
         </thead>
         <tbody>
           <tr>
+            {/* waiting for store data */}
             <td>Onamonapizza</td>
+            {/* waiting for store data */}
             <td>$14.99</td>
           </tr>
           <tr>
+            {/* waiting for store data */}
             <td>Pepperoni</td>
+            {/* waiting for store data */}
             <td>$13.99</td>
           </tr>
         </tbody>
       </Table>
       <p className="total">Total:</p>
       <div className="checkoutButton">
-        <Button className="primaryButton" variant="contained">Checkout</Button>
+        <Button onClick={addOrder} className="primaryButton" variant="contained">Checkout</Button>
       </div>
     </div>
   )

40115ffe0646dc9ed3db5ce0cab87b0ce7fc73d4
Hannah Fligel <fligelhannah@gmail.com>
2021-11-14 14:27:49 -0600

    mapping through all the pizzas and displaying them in the table

--- src/components/CheckOut/CheckOut.js
+++ src/components/CheckOut/CheckOut.js
@@ -4,7 +4,7 @@ import "../Checkout/Checkout.css"
 import {Table}from 'react-bootstrap';
 import {Button} from '@material-ui/core'
 import axios from 'axios';
-
+import CheckoutItem from '../CheckoutItem/CheckoutItem';
 
 
 function Checkout() {
@@ -12,7 +12,11 @@ function Checkout() {
   // const reducerName = useSelector(store => store.reducerName);
   const dispatch = useDispatch();
 
+  //****waiting to change pizzaListReducer to the new store item of pizzas that were added to the cart 
+  const pizzaListReducer = useSelector(store => store.pizzaListReducer);
+
   //create a variable that stores an object for the entire order 
+  //****waiting to get the reducer of customer information from store 
   const[ newOrder, setNewOrder ]=useState({
     customer_name: "Hannah", //waiting for store data
     street_address: "hiawatha ave",//waiting for store data
@@ -44,6 +48,7 @@ function Checkout() {
       <h2>Step 3: Checkout</h2>
       <div className="userInfo-Delivery-Takeout">
         <div className="userInfo">
+          {/* ****waiting to get the reducer of customer information from store  */}
           <p>John Smith</p>
           <p>555 Applewood Lane</p>
           <p>Minneapolis, MN</p>
@@ -61,24 +66,17 @@ function Checkout() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            {/* waiting for store data */}
-            <td>Onamonapizza</td>
-            {/* waiting for store data */}
-            <td>$14.99</td>
-          </tr>
-          <tr>
-            {/* waiting for store data */}
-            <td>Pepperoni</td>
-            {/* waiting for store data */}
-            <td>$13.99</td>
-          </tr>
+          {/* *** waiting to map through the new store item of pizzas that were added to the cart */}
+          {
+            pizzaListReducer.map( pizza =>(<CheckoutItem className="itemBox" pizza={pizza} key={pizza.id}/>))
+          }
         </tbody>
       </Table>
       <p className="total">Total:</p>
       <div className="checkoutButton">
         <Button onClick={addOrder} className="primaryButton" variant="contained">Checkout</Button>
       </div>
+
     </div>
   )
 }

7cd5ac65e7eed5b37826596a63c50515bccf3714
Hannah Fligel <fligelhannah@gmail.com>
2021-11-16 15:06:32 -0600

    added customer info and cart from the store. also added a functioning back button

--- src/components/CheckOut/CheckOut.js
+++ src/components/CheckOut/CheckOut.js
@@ -5,32 +5,32 @@ import {Table}from 'react-bootstrap';
 import {Button} from '@material-ui/core'
 import axios from 'axios';
 import CheckoutItem from '../CheckoutItem/CheckOutItem';
+import { Link } from 'react-router-dom';
+
 
 
 function Checkout() {
-  
-  // const reducerName = useSelector(store => store.reducerName);
-  const dispatch = useDispatch();
 
-  //****waiting to change pizzaListReducer to the new store item of pizzas that were added to the cart 
-  const pizzaListReducer = useSelector(store => store.pizzaListReducer);
+  //bring cart & customer info items from the store 
+  const cart = useSelector(store => store.cart);
+  const customerInfo = useSelector(store => store.customerInfo);
+
 
   //create a variable that stores an object for the entire order 
-  //****waiting to get the reducer of customer information from store 
   const[ newOrder, setNewOrder ]=useState({
-    customer_name: "Hannah", //waiting for store data
-    street_address: "hiawatha ave",//waiting for store data
-    city: "minneapolis",//waiting for store data
-    zip: "55406",//waiting for store data
-    type: "takeout",//waiting for store data
-    total: 20,//waiting for store data
+    customer_name: customerInfo.customer_name, 
+    street_address: customerInfo.street_address,
+    city: customerInfo.city,
+    zip: customerInfo.zip,
+    type: customerInfo.type,
+    total: 20,
     pizzas: [{
-      id: 2, //waiting for store data
-      quantity: 3 //waiting for store data
+      id: 3, 
+      quantity: 3 
     }] 
 })
 
-  //create an axios post req to send over the new order object
+  //create an axios post req to (/api/order) to send over the new order object to the server which will then send it to the db
   const addOrder = ()=>{
     console.log( 'in addItem' );
     axios.post( '/api/order', newOrder ).then ( (response)=>{
@@ -45,16 +45,17 @@ function Checkout() {
 
   return (
     <div className="checkout-page">
+      <Button variant="secondary"><Link to="/customerInfo">back</Link></Button>
       <h2>Step 3: Checkout</h2>
       <div className="userInfo-Delivery-Takeout">
         <div className="userInfo">
-          {/* ****waiting to get the reducer of customer information from store  */}
-          <p>John Smith</p>
-          <p>555 Applewood Lane</p>
-          <p>Minneapolis, MN</p>
+            {/* for customer info, use the newOrder variable with the specific info to display */}
+            <p>{newOrder.customer_name}</p>
+            <p>{newOrder.street_address}</p>
+            <p>{newOrder.city} {newOrder.zip}</p>
         </div>
         <div className="delivery-takeout">
-          <p className="forDelivery">For Delivery/Takeout</p>
+          <p className="forDelivery">{newOrder.type}</p>
         </div>  
       </div>
       
@@ -66,17 +67,18 @@ function Checkout() {
           </tr>
         </thead>
         <tbody>
-          {/* *** waiting to map through the cart that will be added to the store instead of pizzaListReducer */}
+          {/* map through the cart from the store to display each one of the items added */}
           {
-            pizzaListReducer.map( pizza =>(<CheckoutItem className="itemBox" pizza={pizza} key={pizza.id}/>))
+            cart.map( pizza =>(<CheckoutItem className="itemBox" pizza={pizza} key={pizza.id}/>))
           }
         </tbody>
       </Table>
-      <p className="total">Total:</p>
+
+      {/* <------need to add total to the store, currently it's in header and it isn't being dispatched to the store (waiting to do newOrder.total) -----> */}
+      <p className="total">Total:  </p>
       <div className="checkoutButton">
         <Button onClick={addOrder} className="primaryButton" variant="contained">Checkout</Button>
       </div>
-
     </div>
   )
 }
